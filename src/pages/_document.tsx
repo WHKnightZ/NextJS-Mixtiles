8723cba1cacd55a366f030c5702576154c19514e
dealer <nguyenkhanhsl1997@gmail.com>
2022-06-26 22:20:33 +0700

    update title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ function Document() {
   return (
     <Html>
       <Head>
+        <title>Mixtiles - Khung ảnh thông minh</title>
         <meta name="google-site-verification" content="BCLfAyR7Mwxtip9G3mDH1kNR8YjbXLVLSzttx0VtiVg" />
         <meta name="description" content="Mixtiles - Khung ảnh thông minh" />
         <meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no" />
