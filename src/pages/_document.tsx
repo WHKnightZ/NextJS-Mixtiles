2e333bc77802592a6d4eb26b30339426d313e642
dealer <nguyenkhanhsl1997@gmail.com>
2022-06-26 21:27:00 +0700

    add description meta data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ function Document() {
     <Html>
       <Head>
         <meta name="google-site-verification" content="BCLfAyR7Mwxtip9G3mDH1kNR8YjbXLVLSzttx0VtiVg" />
+        <meta name="description" content="Mixtiles - Khung ảnh thông minh" />
+        <meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no" />
+        <meta property="og:image" content="https://i.imgur.com/facvPHX.jpeg" />
       </Head>
       <body>
         <Main />
